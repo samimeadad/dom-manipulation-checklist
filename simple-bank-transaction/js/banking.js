d7c8dc010f829d30552b380f66f06b3b08be0b88
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-08-18 01:24:31 +0600

    validation done Alhamdulillah

--- simple-bank-transaction/js/banking.js
+++ simple-bank-transaction/js/banking.js
@@ -7,13 +7,56 @@ function getUserInput( inputId ) {
     return inputAmount;
 }
 
+//Calculate the Total amount for Deposit and Withdraw and update it
+function updateTotalFiled( totalFieldId, inputAmount ) {
+    const totalField = document.getElementById( totalFieldId );
+    const previousTotalFieldText = totalField.innerText;
+    const previousTotalFieldAmount = parseFloat( previousTotalFieldText );
+    totalField.innerText = previousTotalFieldAmount + inputAmount;
+}
+
+//Get the current balance and return it to the event handler
+function getBalance() {
+    const balanceField = document.getElementById( 'balance-total' );
+    const previouseBalanceFieldText = balanceField.innerText;
+    const previousBalanceFieldAmount = parseFloat( previouseBalanceFieldText );
+    return previousBalanceFieldAmount;
+}
+
+//Calculate the total amount for Balance and update it
+function updateBalanceField( inputAmount, isAdd ) {
+    const balanceField = document.getElementById( 'balance-total' );
+    const previousBalanceAmount = getBalance();
+    if ( isAdd == true ) {
+        balanceField.innerText = previousBalanceAmount + inputAmount;
+    }
+    else {
+        balanceField.innerText = previousBalanceAmount - inputAmount;
+    }
+}
+
+//Event handler for Deposit Button
 document.getElementById( 'deposit-button' ).addEventListener( 'click', function () {
     const inputDepositAmount = getUserInput( 'deposit-input' );
-    
+    if ( inputDepositAmount > 0 ) {
+        updateTotalFiled( 'deposit-total', inputDepositAmount );
+        updateBalanceField( inputDepositAmount, true );
+    }
+    else {
+        alert( 'Wrong User Input!' )
+    }
 } );
 
 
+//Event handler for Withdraw Button
 document.getElementById( 'withdraw-button' ).addEventListener( 'click', function () {
     const inputWithdrawAmount = getUserInput( 'withdraw-input' );
-
+    const previousBalanceAmount = getBalance();
+    if ( inputWithdrawAmount > 0 && previousBalanceAmount > inputWithdrawAmount ) {
+        updateTotalFiled( 'withdraw-total', inputWithdrawAmount );
+        updateBalanceField( inputWithdrawAmount, false );
+    }
+    else {
+        alert( 'Wrong User Input' );
+    }
 } );
